a9813c0d72e35e12e2e6d730e977fe27b90660cf
Bruno Cardoso <bruno@liveonsolutions.com>
2020-08-06 17:28:18 -0300

    add handle submit using callback hook

--- src/pages/SingUp/index.js
+++ src/pages/SingUp/index.js
@@ -1,4 +1,4 @@
-import React, {useRef} from 'react';
+import React, {useRef, useState, useCallback} from 'react';
 
 import Input from '~/components/Input';
 import Button from '~/components/Button';
@@ -9,6 +9,23 @@ function SingUp() {
   const emailRef = useRef(null);
   const passwordRef = useRef(null);
 
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = useCallback(() => {
+    if (username || email || password || loading === true) {
+      return;
+    }
+
+    setLoading(true);
+
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  }, [email, loading, password, username]);
+
   return (
     <Container>
       <Title>Sign Up</Title>
@@ -18,6 +35,8 @@ function SingUp() {
         onSubmitEditing={() => emailRef.current.focus()}
         returnKeyType="next"
         blurOnSubmit={false}
+        value={username}
+        onChangeText={(text) => setUsername(text)}
       />
       <Input
         label="Account information"
@@ -26,9 +45,18 @@ function SingUp() {
         onSubmitEditing={() => passwordRef.current.focus()}
         returnKeyType="next"
         blurOnSubmit={false}
+        keyboardType="email-address"
         style={{marginTop: 20, marginBottom: 10}}
+        value={email}
+        onChangeText={(text) => setEmail(text)}
       />
-      <Input placeholder="Password" fieldRef={passwordRef} secureTextEntry />
+      <Input
+        placeholder="Password"
+        fieldRef={passwordRef}
+        secureTextEntry
+        value={password}
+        onChangeText={(text) => setPassword(text)}
+      />
       <Warning>
         When you registering, you agree to <LinkText>Terms of service</LinkText>{' '}
         and the <LinkText>privacy policy</LinkText> of Discording.
@@ -38,7 +66,7 @@ function SingUp() {
         blue
         text="Create account"
         style={{marginTop: 30}}
-        onPress={() => {}}
+        onPress={handleSubmit}
       />
     </Container>
   );
